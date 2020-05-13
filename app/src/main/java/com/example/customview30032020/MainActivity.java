7263c6fd560b790d47a49ed1527d18496f5e1892
phatdroid94 <phatdroid94@gmail.com>
2020-05-13 20:23:55 +0700

    first commit

---
+++ app/src/main/java/com/example/customview30032020/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.customview30032020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 1 : Tao ra file layout cho view
+        // 2 : Tao ra class dong goi object layout
+    }
+}
